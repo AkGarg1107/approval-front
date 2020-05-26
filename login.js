f84d2db2ebe79892b52c44b1b21af0479ca2382a
Mehul Khandelwal <18ucs096@lnmiit.ac.in>
2020-05-26 10:09:32 +0530

    modified login

--- login.js
+++ login.js
@@ -1,12 +1,19 @@
 function Validate() {
+    
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("passwordverify").value;
     if (password != confirmPassword) {
         alert("Passwords do not match.");
         return false;
     }
+    else if( document.getElementById("email").value=="a@a.a" ){
+        alert("Login with Google Required");
+        return false;
+        
+    }
     return true;
 }
+
 
     
         
